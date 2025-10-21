e73c2cc2d946ac5c4e77d1932e12fae6c344186a
Luana Abrantes <luanar.abrantes@gmail.com>
2025-10-20 22:52:01 -0300

    Colocando front no git

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Login } from './component/login/login';
+import { Register } from './component/register/register';   
+import { Pedido } from './component/pedido/pedido';
+import { NovoPedido } from './component/novo-pedido/novo-pedido';
+import { Produtos } from './component/produtos/produtos';   
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',component: Login
+    },
+    {
+        path: 'register', component: Register,
+    },
+    {
+        path: 'pedido', component: Pedido
+    },
+    {
+        path: 'novo-pedido', component: NovoPedido
+    },
+    {
+        path: 'produtos', component: Produtos
+    }
+ 
+];
